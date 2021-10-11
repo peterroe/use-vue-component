08801b43b4d3581fc22a4abc9e32ca8c253fbec9
Peter roe <peterroe@163.com>
2021-10-11 18:35:01 +0800

    :art: update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,15 @@
  * @Author: 林舒恒
  * @Date: 2021-10-11 18:29:11
  * @LastEditors: 林舒恒
- * @LastEditTime: 2021-10-11 18:29:12
+ * @LastEditTime: 2021-10-11 18:34:02
  */
+const path = require('path')
+
+module.exports = {
+    entry: './index.js'
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    mode: 'production'
+}
